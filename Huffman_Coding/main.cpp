c649269f02033e8e52c2d718c4b6e4bc58131b1d
M.Abdullah Sultan <abdullahsultan448@gmail.com>
2019-11-21 00:02:09 -0800

    Added veector

--- Huffman_Coding/main.cpp
+++ Huffman_Coding/main.cpp
@@ -1,9 +1,32 @@
-#include <stdio.h>
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class   Int_and_Char{
+    
+    Int_and_Char()
+    {count=0;}
+    
+    int count;
+    char character;
+    
+    };
 
 int main(int argc, char **argv)
 {
-    String data;
+    vector<>
+    string data;
     cout<<"Enter data\n";
     cin>>data;
+    int size = data.size();     int count=0;
+    for(int x=0;    x<size;     x++)
+        {
+            for(y=0;    y<size;     y++)
+            {
+                if(x!=y && data[x] == data[y])
+                    count++;
+            }
+        }
+    return 0;
 }
 
