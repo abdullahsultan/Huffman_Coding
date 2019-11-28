81650a3abd56d26fb5a0b83f7ac0079aee7bbe93
M.Abdullah Sultan <abdullahsultan448@gmail.com>
2019-11-27 22:32:08 -0800

    FuckedUp(Wrong Approach)

--- Huffman_Coding/main.cpp
+++ Huffman_Coding/main.cpp
@@ -1,19 +1,50 @@
 #include <bits/stdc++.h> 
+#include <vector>
 using namespace std;
+
 
 class   Int_and_Char{
     
 public:
     Int_and_Char()
-    {count=0;}
+    {count=0; left=NULL; right=NULL;}
     
     Int_and_Char(int cnt,char ch)
-    {count =cnt; character=ch;}
+    {count =cnt; character=ch; left=NULL; right=NULL;}
     
     int count;
     char character;
+    Int_and_Char *left;
+    Int_and_Char *right;
     
     };
+    
+vector<Int_and_Char*> huffmanTree(vector<Int_and_Char*> v)
+{
+        Int_and_Char *s1=new Int_and_Char(INT_MAX,' ');  Int_and_Char *s2 = new Int_and_Char(INT_MAX,' '); int is1,is2;
+        while(v.size()!=1)
+        {
+            for(int x=0;x<v.size();x++)
+            {
+                if(s1->count < v[x]->count)
+                    {s1 = v[x]; is1=x;} 
+            }
+            for(int x=0;x<v.size();x++)
+            {
+                if(s2->count < v[x]->count && v[x]->character != s1->character)
+                    {s2 = v[x];is2=x;} 
+            }
+            vector<Int_and_Char>::iterator  i= v.begin(); 
+            i= &v[is1];
+            v.erase(i); 
+            i = &v[is2];
+            v.erase(i);
+            v.insert(is1,new Int_and_Char(s1->count+s2->count,' ')); v[is1]->left=s1; v[is2]->right=s2;
+            
+            
+        }
+}
+
 
 int main(int argc, char **argv)
 {
