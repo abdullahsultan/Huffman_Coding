f61426affc2867b75ee68e0236fad525969926cf
M.Abdullah Sultan <abdullahsultan448@gmail.com>
2019-11-28 14:44:18 -0800

    New approach

--- Huffman_Coding/main.cpp
+++ Huffman_Coding/main.cpp
@@ -2,6 +2,27 @@
 #include <vector>
 using namespace std;
 
+class Huffman_Tree{
+
+public:
+    int d;
+    char c;
+    Huffman_Tree *left;
+    Huffman_Tree *right;
+    
+        Huffman_Tree(int data, char cha)
+        {
+            d=data; c=cha;
+            left=NULL; right=NULL;
+        }
+        Huffman_Tree()
+        {
+         
+        }
+    
+    };
+
+
 
 
 
@@ -11,6 +32,8 @@ int main(int argc, char **argv)
     
     string data = "abdulh";
     int freq[] = {2,1,1,1,2,1};
+    Huffman_Tree base_array[sizeof(freq)];
+    cout<<sizeof(freq)/sizeof(freq[0]);
 
     return 0;
 }
